daaf5120bfe37471d0e9b90b7649fdc83f67f0ea
Carlo Munguia <carlomunguia@icloud.com>
2021-01-18 16:16:03 -0600

    created bubble sort to power the app! :D

--- build/index.js
+++ build/index.js
@@ -1,5 +1,21 @@
 "use strict";
-console.log('hi there! who oo whoo');
-const logSomething = () => {
-    console.log('something');
-};
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-console.log('hi there! who oo whoo')
+class Sorter {
+  constructor(public collection: number[]) { }
 
-const logSomething = () => {
-  console.log('something')
+  sort(): void {
+    const { length } = this.collection
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j]
+          this.collection[j] = this.collection[j + 1]
+          this.collection[j + 1] = leftHand
+        }
+      }
+    }
+  }
 }
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort()
+console.log(sorter.collection)
