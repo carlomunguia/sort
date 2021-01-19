19aca33d303c680e0781e8becc995a6cd5a86ce8
Carlo Munguia <carlomunguia@icloud.com>
2021-01-18 23:14:44 -0600

    almost at feature complete with sorting node application

--- build/index.js
+++ build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([1000, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([1000, 32, -5, 0])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data)
+const charactersCollection = new CharactersCollection_1.CharactersCollection('XacP');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 
-const numbersCollection = new NumbersCollection([1000, 3, -5, 0])
-const sorter = new Sorter(numbersCollection)
+// const numbersCollection = new NumbersCollection([1000, 32, -5, 0])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data)
+
+const charactersCollection = new CharactersCollection('XacP')
+const sorter = new Sorter(charactersCollection)
 sorter.sort()
-console.log(numbersCollection.data)
+console.log(charactersCollection.data)
