e9ec0b0f3bf5ae1cbe0827cac6266748fc52e706
Carlo Munguia <carlomunguia@icloud.com>
2021-01-18 22:11:53 -0600

    created NumbersCollection class, wired things together between Sorter, index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) { }
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
 
-  sort(): void {
-    const { length } = this.collection
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j]
-          this.collection[j] = this.collection[j + 1]
-          this.collection[j + 1] = leftHand
-        }
-      }
-    }
-  }
-}
-const sorter = new Sorter([10, 3, -5, 0])
+const numbersCollection = new NumbersCollection([1000, 3, -5, 0])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
